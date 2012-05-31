73376e55aa13368d072a7ad37ed685a7ef954741
chris <chris@linnie.(none)>
2012-05-31 12:39:19 -0400

    merged

--- project/core/src/main/java/com/domain/project/core/Camera.java
+++ project/core/src/main/java/com/domain/project/core/Camera.java
@@ -5,45 +5,45 @@ import playn.core.Image.Region;
 
 public class Camera
 {
-	private float xOffset;
-	private float yOffset;
-	private float weight = Const.WINDOW_WIDTH;
-	private float height = Const.WINDOW_HEIGHT;
-	
-	private Region view;
-	
-	public Camera(float xOffset, float yOffset, Image bgImage)
-	{
-		this.xOffset = xOffset;
-		this.yOffset = yOffset;
-		view = bgImage.subImage(xOffset, yOffset, weight, height);
-	}
-	
-	public void setX(float xOffset)
-	{
-		this.xOffset = xOffset;
-		view.setBounds(xOffset, yOffset, weight, height);
-	}
-	
-	public void setY(float yOffset)
-	{
-		this.yOffset = yOffset;
-		view.setBounds(xOffset, yOffset, weight, height);
-	}
-	
-	public Region getView()
-	{
-		return view;
-	}
-	
-	public float getX()
-	{
-		return xOffset;
-	}
-	
-	public float getY()
-	{
-		return yOffset;
-	}
+    private float xOffset;
+    private float yOffset;
+    private float width = Const.WINDOW_WIDTH;
+    private float height = Const.WINDOW_HEIGHT;
+
+    private Region view;
+
+    public Camera(float xOffset, float yOffset, Image bgImage)
+    {
+        this.xOffset = xOffset;
+        this.yOffset = yOffset;
+        view = bgImage.subImage(xOffset, yOffset, width, height);
+    }
+
+    public void setX(float xOffset)
+    {
+        this.xOffset = xOffset;
+        view.setBounds(xOffset, yOffset, width, height);
+    }
+
+    public void setY(float yOffset)
+    {
+        this.yOffset = yOffset;
+        view.setBounds(xOffset, yOffset, width, height);
+    }
+
+    public Region getView()
+    {
+        return view;
+    }
+
+    public float getX()
+    {
+        return xOffset;
+    }
+
+    public float getY()
+    {
+        return yOffset;
+    }
 
 }
--- project/core/src/main/java/com/domain/project/core/GameLoop.java
+++ project/core/src/main/java/com/domain/project/core/GameLoop.java
@@ -3,6 +3,7 @@ package com.domain.project.core;
 import static playn.core.PlayN.*;
 
 import playn.core.Game;
+import playn.core.GroupLayer;
 import playn.core.Image;
 import playn.core.ImageLayer;
 
@@ -10,62 +11,65 @@ import playn.core.Keyboard;
 import playn.core.Key;
 
 public class GameLoop implements Game {
-  @Override
-  public void init() {
-	//set window size
-	graphics().setSize(Const.WINDOW_WIDTH,Const.WINDOW_HEIGHT);
-    // create and add background image layer
-    final Image bgImage = assets().getImage("images/sample_environment.jpg");
-	final Camera camera = new Camera(0,0, bgImage);
-    final ImageLayer bgLayer = graphics().createImageLayer(camera.getView());
-	bgLayer.setSize(3200,1800);
-    graphics().rootLayer().add(bgLayer);
 
-    System.out.println(bgImage.width() + " " + bgImage.height());
-    //bgLayer.setSize(graphics().width(), graphics().height());
-	
-	// add a listener for Keyboard input
-    keyboard().setListener(new Keyboard.Adapter() {
-      @Override
-      public void onKeyDown(Keyboard.Event event) {
-        if(event.key().equals(Key.A))
-		    camera.setX(camera.getX() - 20);
-		if(event.key().equals(Key.W))
-			camera.setY(camera.getY() - 20);
-		if(event.key().equals(Key.D))
-		    camera.setX(camera.getX() + 20);
-		if(event.key().equals(Key.S))
-			camera.setY(camera.getY() + 20);
-		if(event.key().equals(Key.ESCAPE))
-			System.exit(0);
-      }
-	  /*@Override
-      public void onKeyUp(Keyboard.Event event) {
-        if(event.key().equals(Key.A))
-		    camera.setX(camera.getX() - 20);
-		if(event.key().equals(Key.W))
-			camera.setY(camera.getY() - 20);
-		if(event.key().equals(Key.D))
-		    camera.setX(camera.getX() + 20);
-		if(event.key().equals(Key.S))
-			camera.setY(camera.getY() + 20);
-      }*/
-    });
+    GroupLayer graph_layer;
+
+    @Override
+    public void init() {
+        //set window size
+        graphics().setSize(Const.WINDOW_WIDTH,Const.WINDOW_HEIGHT);
+        // create and add background image layer
+        final Image bgImage = assets().getImage("images/sample_environment.jpg");
+        final Camera camera = new Camera(0,0, bgImage);
+        final ImageLayer bgLayer = graphics().createImageLayer(camera.getView());
+        bgLayer.setSize(3200,1800);
+        graphics().rootLayer().add(bgLayer);
+
+        System.out.println(bgImage.width() + " " + bgImage.height());
+        //bgLayer.setSize(graphics().width(), graphics().height());
+
+        // add a listener for Keyboard input
+        keyboard().setListener(new Keyboard.Adapter() {
+            @Override
+            public void onKeyDown(Keyboard.Event event) {
+                if(event.key().equals(Key.A))
+                    camera.setX(camera.getX() - 20);
+                if(event.key().equals(Key.W))
+                    camera.setY(camera.getY() - 20);
+                if(event.key().equals(Key.D))
+                    camera.setX(camera.getX() + 20);
+                if(event.key().equals(Key.S))
+                    camera.setY(camera.getY() + 20);
+                if(event.key().equals(Key.ESCAPE))
+                    System.exit(0);
+          }
+          /*@Override
+          public void onKeyUp(Keyboard.Event event) {
+            if(event.key().equals(Key.A))
+                camera.setX(camera.getX() - 20);
+            if(event.key().equals(Key.W))
+                camera.setY(camera.getY() - 20);
+            if(event.key().equals(Key.D))
+                camera.setX(camera.getX() + 20);
+            if(event.key().equals(Key.S))
+                camera.setY(camera.getY() + 20);
+          }*/
+        });
     
-  }
+    }
 
-  @Override
-  public void paint(float alpha) {
-    // the background automatically paints itself, so no need to do anything here!
-  }
+    @Override
+    public void paint(float alpha) {
+       // the background automatically paints itself, so no need to do anything here!
+    }
 
-  @Override
-  public void update(float delta) {
-  
-  }
+    @Override
+    public void update(float delta) {
+    
+    }
 
-  @Override
-  public int updateRate() {
-    return 25;
-  }
+    @Override
+    public int updateRate() {
+        return 25;
+    }
 }
