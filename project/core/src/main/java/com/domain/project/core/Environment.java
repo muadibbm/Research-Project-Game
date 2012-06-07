14bc4ab64cf635068af0409d13a5a41e1c986429
chris <chris@linnie.(none)>
2012-06-07 17:53:47 -0400

    scrolling environment

--- project/core/src/main/java/com/domain/project/core/Environment.java
+++ project/core/src/main/java/com/domain/project/core/Environment.java
@@ -8,42 +8,50 @@ import playn.core.ImageLayer;
 
 public class Environment
 {
-	private GroupLayer graphLayer; //contains graph objects(city graphs, camp graphs, etc)
-	private ImageLayer backGround;
-	private GroupLayer userInterface; //contains all the objects and layers for the game UI
-	//TODO : any more layers ?
-	
-	public Environment()
-	{
-		//set window size
+    private GroupLayer graphLayer; //contains graph objects(city graphs, camp graphs, etc)
+    private ImageLayer bgLayer;
+    private GroupLayer uiLayer; //contains all the objects and layers for the game UI
+    //TODO : any more layers ?
+
+    public Environment()
+    {
+        //set window size
         graphics().setSize(Const.WINDOW_WIDTH,Const.WINDOW_HEIGHT);
-		//TODO : Each device has different screen parameters (see http://playn-2011.appspot.com/slides/index.html#19)
-		
-		// create background image layer
+        //TODO : Each device has different screen parameters (see http://playn-2011.appspot.com/slides/index.html#19)
+
+
+       // create background image layer
         Image bgImage = assets().getImage("images/texture_wall_painted11.png");
-		//Image image = assets().getImage("images/sample_environment.jpg");
-		backGround = graphics().createImageLayer(bgImage);//TODO: camera.getView()
-		backGround.setSize(Const.WORLD_WIDTH,Const.WORLD_HEIGHT);
-		backGround.setRepeatX(true);
-		backGround.setRepeatY(true);
-		
+        //Image image = assets().getImage("images/sample_environment.jpg");
+        bgLayer = graphics().createImageLayer(bgImage);//TODO: camera.getView()
+        bgLayer.setSize(Const.WORLD_WIDTH,Const.WORLD_HEIGHT);
+        bgLayer.setRepeatX(true);
+        bgLayer.setRepeatY(true);
+
         //create group layer containing the graphs
         graphLayer = graphics().createGroupLayer();
-		//create group layer containing the UI
-		userInterface = graphics().createGroupLayer();
-		//add all the layer to the root Layer
-		graphics().rootLayer().add(backGround);
+        //create group layer containing the UI
+        uiLayer = graphics().createGroupLayer();
+        //add all the layer to the root Layer
+        graphics().rootLayer().add(bgLayer);
         graphics().rootLayer().add(graphLayer);
-		//graphics().rootLayer().add(userInterface);
-	}
-	
-	public Image getBaseImage()
-	{
-		return backGround.image();
-	}
-	
-	public GroupLayer getGraphLayer()
-	{
-		return graphLayer;
-	}
+        //graphics().rootLayer().add(uiLayer);
+
+        graphLayer.add(bgLayer);
+
+    }
+
+    public Image getBaseImage()
+    {
+        return bgLayer.image();
+    }
+
+    public GroupLayer getGraphLayer()
+    {
+        return graphLayer;
+    }
+
+    public void updateView(float x, float y) {
+        graphLayer.setOrigin(x, y);
+    }
 }
--- project/core/src/main/java/com/domain/project/core/GameLoop.java
+++ project/core/src/main/java/com/domain/project/core/GameLoop.java
@@ -10,22 +10,22 @@ import playn.core.Keyboard;
 import playn.core.Key;
 
 public class GameLoop implements Game {
-	
+    
     Camera camera;
-	
-	Environment environmet;
+    
+    Environment environment;
     
     KeyboardControls kbControls;
 
     @Override
     public void init() {
-		
-		environmet = new Environment();
-        camera = new Camera(0,0, environmet.getBaseImage());
-		
-		Graph g = new Graph();
-        g.generateGraph("3IZ9", environmet.getGraphLayer());
-		
+        
+        environment = new Environment();
+        camera = new Camera(0,0, environment.getBaseImage());
+        
+        Graph g = new Graph();
+        g.generateGraph("3IZ9", environment.getGraphLayer());
+        
         //create and set keyboard controls
         kbControls = new KeyboardControls();
         keyboard().setListener(kbControls);
@@ -38,32 +38,33 @@ public class GameLoop implements Game {
 
     @Override
     public void update(float delta) {
-        parseKB();
+        //parseKB();
+        environment.updateView(camera.getX(), camera.getY());
     }
 
     @Override
     public int updateRate() {
         return 25;
     }
-
-    private void parseKB() {
-        //parse keyboard inputs
-        if(kbControls.scrollUp) {
-			if(camera.getY() > 0)
-				camera.setY(camera.getY() - kbControls.panRate);
-        }
-        if(kbControls.scrollDown) {
-			if(camera.getY() < Const.WORLD_HEIGHT)
-				camera.setY(camera.getY() + kbControls.panRate);
-        }
-        if(kbControls.scrollLeft) {
-			if(camera.getX() > 0)
-				camera.setX(camera.getX() - kbControls.panRate);
-        }
-        if(kbControls.scrollRight) {
-			if(camera.getX() < Const.WORLD_WIDTH)
-				camera.setX(camera.getX() + kbControls.panRate);
-        }
-    }
+//
+//    private void parseKB() {
+//        //parse keyboard inputs
+//        if(kbControls.scrollUp) {
+//            if(camera.getY() > 0)
+//                camera.setY(camera.getY() - kbControls.panRate);
+//        }
+//        if(kbControls.scrollDown) {
+//            if(camera.getY() < Const.WORLD_HEIGHT)
+//                camera.setY(camera.getY() + kbControls.panRate);
+//        }
+//        if(kbControls.scrollLeft) {
+//            if(camera.getX() > 0)
+//                camera.setX(camera.getX() - kbControls.panRate);
+//        }
+//        if(kbControls.scrollRight) {
+//            if(camera.getX() < Const.WORLD_WIDTH)
+//                camera.setX(camera.getX() + kbControls.panRate);
+//        }
+//    }
 
 }
