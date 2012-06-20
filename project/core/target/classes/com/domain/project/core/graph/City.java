3d4f31576637860bd071d8f65c7d4c84e71cfad0
unknown <muadibbm@HAL.(none)>
2012-06-19 22:38:54 -0400

    Fixed Layer

---
+++ project/core/target/classes/com/domain/project/core/graph/City.java
@@ -0,0 +1,20 @@
+package com.domain.project.core.graph;
+
+import playn.core.ImageLayer;
+import playn.core.GroupLayer;
+
+public class City extends Base
+{
+	private ImageLayer townHall;
+	private ImageLayer bazar;
+	private ImageLayer garden;
+	private ImageLayer tower;
+	private ImageLayer wall;
+	//TODO: how to position the city and its componants
+
+	public City(GroupLayer graphLayer){
+		super(graphLayer);
+	}
+	
+	//TODO: add setters and getters
+}
