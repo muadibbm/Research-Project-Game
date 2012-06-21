ccffa599ca9b268548f96c6c129d826467a57292
chris <cwong010@gmail.com>
2012-06-21 16:52:01 -0400

    merging

---
+++ project/core/src/main/java/com/domain/project/core/controls/Controls.java
@@ -0,0 +1,12 @@
+package com.domain.project.core.controls;
+
+public class Controls {
+
+//    protected static boolean 
+    protected static float scaleFactor = 1.0f;
+    protected static float scaleRate = 0.1f;
+    protected static final float scaleFactorMax = 5.0f;
+
+
+
+}
